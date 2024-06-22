d058cea20495d3823df44e700e66bf88c4a3854a
Kadia-D <kddellor@gmail.com>
2024-06-22 20:00:54 +0000

    final commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require ('mongoose')
-const ShortUrl = require('./models/ShortUrl')
+const ShortUrl = require('./models/shortUrl')
+const shortUrl = require('./models/shortUrl')
 const app = express()
 
 mongoose.connect('mongodb://localhost/urlShortener', {
@@ -10,12 +11,24 @@ mongoose.connect('mongodb://localhost/urlShortener', {
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false }))
 
-app.get('/' , (req, res) =>{
-    res.render('index')
+app.get('/' , async (req, res) =>{
+    const shortUrls =await shortUrl.find()
+    res.render('index',  {shortUrls: shortUrls})
 })
 
-app.post('/shortUrls', (req, res) =>{
-    ShortUrl.create({full: req.body.fullUrl})
+app.post('/shortUrls', async (req, res) =>{
+   await ShortUrl.create({full: req.body.fullUrl})
+   res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+    if (shortUrl == null) return res.sendStatus(404)
+
+        shortUrl.clicks ++
+        shortUrl.save()
+
+        res.redirect(shortUrl.full)
 })
 
 app.listen(process.env.PORT || 5000);
